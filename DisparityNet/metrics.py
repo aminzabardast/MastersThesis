bfc6c5375ba1a72c97cf4050bafef07e9a245d81
Amin Zabardast <aminzabardast@gmail.com>
2018-10-14 01:36:52 +0300

    Numpy to Tensorflow Fixes #23 Changing simple Numpy calculations into Tensorflow computational tensor calculations.

--- DisparityNet/metrics.py
+++ DisparityNet/metrics.py
@@ -1,12 +1,13 @@
-import numpy as np
+import tensorflow as tf
 
 
 def bad_n(y_true, y_pred, n):
     """Returning percentage of pixels with more than n units of disparity in errors"""
-    diff = np.subtract(y_true, y_pred)
-    diffn = np.sum(np.abs(diff) > n)
-    total = np.multiply(*diff.shape)
-    return np.divide(np.multiply(diffn, 100), total)
+    diff = tf.abs(tf.subtract(y_true, y_pred))
+    diff_n = tf.reduce_sum(tf.cast(diff > n, dtype=tf.float32), axis=[1, 2, 3])
+    total = tf.cast(tf.multiply(diff.shape[1], diff.shape[2]), dtype=tf.float32)
+    r = tf.cast(tf.divide(tf.cast(tf.multiply(diff_n, tf.constant(100, dtype=tf.float32)), dtype=tf.float32), total), dtype=tf.float32)
+    return r
 
 
 def bad_4_0(y_true, y_pred):
