3bb18424db2466ad7ab410d2756f9b5436212d58
amin <aminzabardast@gmail.com>
2019-01-31 22:00:48 +0300

    Modification to CS type network Refs #54

--- DisparityNet/src/disparitynet_cs/disparitynet_cs.py
+++ DisparityNet/src/disparitynet_cs/disparitynet_cs.py
@@ -6,23 +6,28 @@ from ..flow_warp import warp_layer
 
 class DisparityNetCS(BaseNetwork):
 
-    def __init__(self, epochs=1, name_prefix='cs'):
+    def __init__(self, epochs=1, name_prefix='cs', output_channels=1):
         super(DisparityNetCS, self).__init__()
         self.name = 'disparitynet_cs'
         self.epochs = epochs
-        self.disparitynet_c = DisparityNetC(self.epochs, name_prefix='{}/c'.format(name_prefix))
-        self.disparitynet_s = DisparityNetS(self.epochs, name_prefix='{}/s'.format(name_prefix))
+        self.disparitynet_c = DisparityNetC(self.epochs,
+                                            name_prefix='{}/c'.format(name_prefix),
+                                            output_channels=2,
+                                            large_displacement=True)
+        self.disparitynet_s = DisparityNetS(self.epochs,
+                                            name_prefix='{}/s'.format(name_prefix),
+                                            output_channels=output_channels)
 
     def model(self, *args, **kwargs):
 
-        initial_disparity = self.disparitynet_c.model(left_input=kwargs['left_input'],
-                                                      right_input=kwargs['right_input'])
-        warped_left_input = warp_layer(kwargs['right_input'], initial_disparity)
-        disparity = self.disparitynet_s.model(left_input=kwargs['left_input'],
-                                              warped_left_input=warped_left_input,
-                                              disparity=initial_disparity,
-                                              right_input=kwargs['right_input'])
-        return disparity
+        disparity_c = self.disparitynet_c.model(left_input=kwargs['left_input'],
+                                                right_input=kwargs['right_input'])
+        warped_left_input_c = warp_layer(kwargs['right_input'], disparity_c)
+        disparity_s = self.disparitynet_s.model(left_input=kwargs['left_input'],
+                                                warped_left_input=warped_left_input_c,
+                                                disparity=disparity_c,
+                                                right_input=kwargs['right_input'])
+        return disparity_s
 
     def loss(self, *args, **kwargs):
         return 'logcosh'
