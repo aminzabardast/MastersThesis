df0a216b441a0d82055331fb33628f2657d765be
amin <aminzabardast@gmail.com>
2019-01-31 21:58:09 +0300

    modifying the C type network Refs #54

--- DisparityNet/src/disparitynet_c/disparitynet_c.py
+++ DisparityNet/src/disparitynet_c/disparitynet_c.py
@@ -1,15 +1,17 @@
 from tensorflow.keras.layers import Conv2D, concatenate, Conv2DTranspose
 from src.model import BaseNetwork
-from ..correlation import correlation_layer
+from ..correlation import correlation_layer_large_displacement, correlation_layer_small_displacement
 
 
 class DisparityNetC(BaseNetwork):
 
-    def __init__(self, epochs=1, name_prefix='c'):
+    def __init__(self, epochs=1, large_displacement=True, name_prefix='c', output_channels=1):
         super(DisparityNetC, self).__init__()
         self.name = 'disparitynet_c'
         self.epochs = epochs
         self.name_prefix = name_prefix
+        self.output_channels = output_channels
+        self.large_displacement = large_displacement
 
     def model(self, *args, **kwargs):
         # Creating Encoder
@@ -22,7 +24,10 @@ class DisparityNetC(BaseNetwork):
         conv_b_2 = Conv2D(kernel_size=(5, 5), strides=2, filters=128, padding='same', activation='relu', name='{}/conv_b_2'.format(self.name_prefix))(conv_b_1)  # Resulting Dimensions: 128x128x128
         conv_b_3 = Conv2D(kernel_size=(5, 5), strides=2, filters=256, padding='same', activation='relu', name='{}/conv_b_3'.format(self.name_prefix))(conv_b_2)  # Resulting Dimensions: 64x64x256
 
-        cc = correlation_layer(conv_a_3, conv_b_3)  # Resulting Dimensions: 64x64x441
+        if self.large_displacement:
+            cc = correlation_layer_large_displacement(conv_a_3, conv_b_3)  # Resulting Dimensions: 64x64x?
+        else:
+            cc = correlation_layer_small_displacement(conv_a_3, conv_b_3)  # Resulting Dimensions: 64x64x?
 
         conv_redir = Conv2D(kernel_size=(1, 1), strides=1, filters=32, padding='same', activation='relu', name='{}/conv_redir'.format(self.name_prefix))(conv_a_3)  # Resulting Dimensions: 64x64x32
 
@@ -31,7 +36,7 @@ class DisparityNetC(BaseNetwork):
         conv3_1 = Conv2D(kernel_size=(3, 3), strides=1, filters=256, padding='same', activation='relu', name='{}/conv_3_1'.format(self.name_prefix))(cc_concat)  # Resulting Dimensions: 64x64x256
 
         conv4 = Conv2D(kernel_size=(3, 3), strides=2, filters=512, padding='same', activation='relu', name='{}/conv_4'.format(self.name_prefix))(conv3_1)  # Resulting Dimensions: 32x32x512
-        conv4_1 = Conv2D(kernel_size=(3, 3), strides=1, filters=512, padding='same', activation='relu', name='{}/conv_4_1')(conv4)  # Resulting Dimensions: 32x32x512
+        conv4_1 = Conv2D(kernel_size=(3, 3), strides=1, filters=512, padding='same', activation='relu', name='{}/conv_4_1'.format(self.name_prefix))(conv4)  # Resulting Dimensions: 32x32x512
 
         conv5 = Conv2D(kernel_size=(3, 3), strides=2, filters=512, padding='same', activation='relu', name='{}/conv_5_1'.format(self.name_prefix))(conv4_1)  # Resulting Dimensions: 16x16x512
         conv5_1 = Conv2D(kernel_size=(3, 3), strides=1, filters=512, padding='same', activation='relu', name='{}/conv_5_2'.format(self.name_prefix))(conv5)  # Resulting Dimensions: 16s16x512
@@ -54,7 +59,7 @@ class DisparityNetC(BaseNetwork):
         deconv1 = Conv2DTranspose(filters=32, kernel_size=(5, 5), strides=2, padding='same', activation='relu', name='{}/deconv1'.format(self.name_prefix))(deconv2_concat)  # Resulting Dimensions: 256x256x32
         deconv1_concat = concatenate([deconv1, conv_a_1])
 
-        deconv0 = Conv2DTranspose(filters=2, kernel_size=(5, 5), strides=2, padding='same', activation='relu', name='{}/deconv0'.format(self.name_prefix))(deconv1_concat)  # Resulting Dimensions: 512x512x1
+        deconv0 = Conv2DTranspose(filters=self.output_channels, kernel_size=(5, 5), strides=2, padding='same', activation='relu', name='{}/deconv0'.format(self.name_prefix))(deconv1_concat)  # Resulting Dimensions: 512x512x1
         return deconv0
 
     def loss(self, *args, **kwargs):
