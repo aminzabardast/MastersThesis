a32a907174ab931254bf10195ec2a444032adb6b
amin <aminzabardast@gmail.com>
2019-02-17 19:55:12 +0300

    Fixing Callbacks Refs #67

--- DisparityNet/src/callbacks.py
+++ DisparityNet/src/callbacks.py
@@ -1,29 +1,34 @@
 from tensorflow.keras.callbacks import Callback
 import csv
 from shutil import copy2 as copy
-from os import remove
-from os.path import exists
+from os import remove, mkdir
+from os.path import exists, join, isdir
 
 
 class EpochCSVLogger(Callback):
-    def __init__(self, filename, delimiter=',', append=False):
-        self.filename = filename
+    def __init__(self, csv_dir, file_name='epoch.log.csv', delimiter=',', append=False):
+        self.csv_dir = csv_dir
+        self.file_name = file_name
         self.delimiter = delimiter
         self.append = append
         self.file = None
         self.csv = None
-        self.is_initial = not exists(self.filename)
+        self.is_initial = not exists(join(self.csv_dir, self.file_name))
         super(EpochCSVLogger, self).__init__()
 
+    def create_dir(self):
+        if not isdir(self.csv_dir):
+            mkdir(self.csv_dir)
+
     def create_file(self):
-        self.file = open(self.filename, 'a' if self.append else 'w')
+        self.file = open(join(self.csv_dir, self.file_name), 'a' if self.append else 'w')
         self.csv = csv.writer(self.file)
 
     def create_backups(self):
-        copy(self.filename, self.filename+'.bak')
+        copy(join(self.csv_dir, self.file_name), join(self.csv_dir, self.file_name + '.bak'))
 
     def remove_backups(self):
-        remove(self.filename+'.bak')
+        remove(join(self.csv_dir, self.file_name + '.bak'))
 
     def write_header(self, logs):
         if self.is_initial:
@@ -32,6 +37,7 @@ class EpochCSVLogger(Callback):
             self.is_initial = False
 
     def on_train_begin(self, logs=None):
+        self.create_dir()
         self.create_file()
         self.create_backups()
 
@@ -46,24 +52,29 @@ class EpochCSVLogger(Callback):
 
 
 class BatchCSVLogger(Callback):
-    def __init__(self, filename, delimiter=',', append=False):
-        self.filename = filename
+    def __init__(self, csv_dir, file_name='batch.log.csv', delimiter=',', append=False):
+        self.csv_dir = csv_dir
+        self.file_name = file_name
         self.delimiter = delimiter
         self.append = append
         self.file = None
         self.csv = None
-        self.is_initial = not exists(self.filename)
+        self.is_initial = not exists(join(self.csv_dir, self.file_name))
         super(BatchCSVLogger, self).__init__()
 
+    def create_dir(self):
+        if not isdir(self.csv_dir):
+            mkdir(self.csv_dir)
+
     def create_file(self):
-        self.file = open(self.filename, 'a' if self.append else 'w')
+        self.file = open(join(self.csv_dir, self.file_name), 'a' if self.append else 'w')
         self.csv = csv.writer(self.file)
 
     def create_backups(self):
-        copy(self.filename, self.filename+'.bak')
+        copy(join(self.csv_dir, self.file_name), join(self.csv_dir, self.file_name+'.bak'))
 
     def remove_backups(self):
-        remove(self.filename+'.bak')
+        remove(join(self.csv_dir, self.file_name+'.bak'))
 
     def write_header(self, logs):
         if self.is_initial:
@@ -72,6 +83,7 @@ class BatchCSVLogger(Callback):
             self.is_initial = False
 
     def on_train_begin(self, logs=None):
+        self.create_dir()
         self.create_file()
         self.create_backups()
 
